357688f6717cc8067b0285a9baac8dc9cfe8843a
abhrajitray77 <abhrajitray77@gmail.com>
2022-12-16 20:38:03 +0530

    Navbar basic done

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,20 +4,22 @@ import logo from "../../public/imgs/enactus.png"
 
 const Header = () => {
   return (
-    <header className=" bg-slate-600">
-      <div className="flex items-center py-6">
+    <header className=" bg-slate-300/30 z-10 outline outline-slate-200 outline-2
+    rounded-b-2xl backdrop-blur-sm shadow-slate-100 shadow-[inset_0_4px_12px]  ">
+      <div className="flex items-center py-4 space-x-2 md:px-4 lg:px-6 md:space-x-10">
         <Image
           className="cursor-pointer object-contain"
           src={logo}
-          height={50}
-          width={60}
+          height={100}
+          width={100}
           alt="enactus_logo"
         />
 
-        <ul className="flex px-6 space-x-4">
+        <ul className="flex md:px-2 lg:px-6 space-x-10 text-slate-100 font-medium 
+        cursor-pointer lg:space-x-20">
           <li>Who we are</li>
           <li>What we do</li>
-          <li>Our Team</li>
+          <li>Our Teams</li>
         </ul>
       </div>
     </header>
