bd9a6eec81bdeb3f08e6abde70d8ce609c1ef347
abhrajitray77 <abhrajitray77@gmail.com>
2023-01-14 00:07:16 +0530

    added smooth scroll, custom scrollbar, newIcon

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { Bars3Icon } from '@heroicons/react/24/solid';
-import logo from "../../public/imgs/enactus.png";
+import logo from "../../public/imgs/Enactus-SMIT-dark.png";
 import Link from "next/link";
 import teams from "../pages/teams";
 import { useRouter } from "next/router";
@@ -21,19 +21,19 @@ const Header = () => {
     } else {
       setTimeout(() => {
         setDisplayMenu(true);
-      }, 200);
+      }, 100);
     }
   };
 
   return (
-    <header className="relative flex flex-row py-4 bg-slate-800 z-10 backdrop-blur-sm w-auto">
-      <div className="flex items-center md:px-4 lg:px-6 md:space-x-10">
+    <header className="relative flex flex-row py-1 bg-slate-800 z-10 backdrop-blur-sm w-auto">
+      <div className="flex items-center md:px-4 lg:px-2 md:space-x-2">
         <Image
           onClick={()=> router.push('/')}
           className="cursor-pointer object-contain"
           src={logo}
-          height={50}
-          width={50}
+          height={60}
+          width={110}
           alt="enactus_logo"
         />
 
@@ -45,7 +45,7 @@ const Header = () => {
           <li><Link href="/updates">Updates</Link></li>
         </ul>
       </div>
-      <div className="hidden md:inline md:absolute md:py-4 md:px-4 md:inset-y-0 md:right-0
+      <div className=" hidden md:inline md:absolute md:py-4 md:px-4 md:inset-y-0 md:right-0
       ">
         <button className=" bg-yellow-400 rounded-md p-3 text-sm font-medium text-slate-800
         hover:text-black hover:shadow-[0_1px_4px_4px] hover:shadow-yellow-300 hover:transition hover:duration-[.3s]">
@@ -57,11 +57,11 @@ const Header = () => {
       <div className="flex absolute inset-y-0 right-0 top-0 md:hidden">
         <nav className={`${isOpen ? "w-[45vw]" : "w-0"} duration-[.3s] h-screen bg-slate-600/90`}>
         <ul className={`absolute right-0 mr-6 mt-[25vw] space-y-10 text-slate-100 font-medium
-        md:hidden ${displayMenu ? "inline-block" : "hidden"}`}>
-            <li>Who we are</li>
-            <li>What we do</li>
-            <li>Our Teams</li>
-            <li>Updates</li>
+        md:hidden ${displayMenu ? "inline-block" : "hidden"}`} onClick={toggleMenu}>
+          <li><Link href="/who">Who we are</Link></li>
+          <li><Link href="/whatWe">What we do</Link></li>
+          <li><Link href="/teams">Our Teams</Link></li>
+          <li><Link href="/updates">Updates</Link></li>
           </ul>
         </nav>
       </div>
@@ -69,7 +69,7 @@ const Header = () => {
 
       {/*nav button*/}
 
-      <div className="inline absolute py-6 px-4￼ inset-y-0 right-0 text-sm font-medium md:hidden cursor-pointer">
+      <div className="inline absolute py-6 px-4 inset-y-0 right-0 text-sm font-medium md:hidden cursor-pointer" >
         <Bars3Icon className={`h-6 w-6 mr-3 fill-yellow-300 transition-transform duration-[.6] ${isOpen ? "rotate-90": "rotate-0"}`} 
         onClick={toggleMenu} />
       </div>
