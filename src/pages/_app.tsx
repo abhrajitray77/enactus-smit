1537cb4fcda761e59d7e7ef403f615e08dc50d87
abhrajitray77 <abhrajitray77@gmail.com>
2023-01-23 22:33:36 +0530

    Added footer and social media links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../Components/Header";
 import Backdrop from "../Components/Backdrop";
-import '../styles/globals.css'
+import Footer from "../Components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <div className="">    
         <Header />
         <Component {...pageProps} />
+        <Footer />
       </div>
         );
 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -11,7 +11,7 @@ const projects = () => {
         <span className="absolute h-[130px] w-[41%] mt-[520px] right-0 bg-lime-600"></span>
         <span className="absolute h-[440px] w-[15%] mt-[150px] right-[374px] bg-lime-600"></span>
       </div>
-       <section className="relative flex md:flex-row flex-col">
+       <section className="relative flex md:flex-row flex-col py-10 md:py-0">
         <article className="flex flex-col ml-8 md:ml-14 mt-16 mb-10 lg:flex lg:flex-col lg:ml-20 w-[80%] md:w-[65%] lg:w-[50%] lg:mt-16 lg:mb-24 z-10">
           <h1 className="whitespace-nowrap text-[3rem] md:text-[3.5rem] lg:text-[5.2rem] font-semibold text-slate-900">अन्न-Drishti</h1>
           <p className="text-justify md:text-[1.2rem] lg:text-[1.5rem] text-gray-900 font-medium indent-20 mt-8 leading-relaxed">
